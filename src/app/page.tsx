42a25af71f53685d9dbec8c649e2276f95c076cd
Jonathan <many.d.r.o.2001@hotmail.com>
2025-07-10 17:12:07 -0500

    Main title with its corresponding description implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center flex-col">
-      <h1 className="text-4xl text-accent hover:text-accent-hover font-bold ">
-        Homepage
-      </h1>
+    <main className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24 xl:px-8">
+          {/* Text */}
+          <div className="text-center xl:text-left">
+            <span className="capitalize text-2xl">
+              software developer
+            </span>
+            <h1 className="flex flex-col text-center xl:text-left text-7xl h1">
+              <span>Hello I&apos;m </span>
+              <span className="text-accent">Jonathan Olivos</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80 place-self-center xl:place-self-start">
+              I excel at crafting elegant digital experiences and I am
+              proficient in various software technologies
+            </p>
+            <Button variant="outline" size="lg">
+              <span>Download CV</span>
+              <FiDownload className="text-xl" />
+            </Button>
+          </div>
+          <div>Socials</div>
+          <div>Photo</div>
+        </div>
+      </div>
     </main>
   );
 }
